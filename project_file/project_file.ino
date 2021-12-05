f886def977ddb72c2d49a5eed102c7b1b96a2fef
LanfearDB <lanfeadb@udmercy.edu>
2021-12-04 23:12:39 -0500

    Update project_file.ino
    organized functions, added WIFI sending/receiving

--- project_file/project_file.ino
+++ project_file/project_file.ino
@@ -7,11 +7,11 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-int led_state = LOW;    // the current state of LED
-int button_state;       // the current state of button
-int last_button_state;  // the previous state of button
+uint8_t led_state = LOW;    // the current state of LED
+uint8_t button_state;       // the current state of button
+uint8_t last_button_state;  // the previous state of button
 bool isBluetoothOn = false;
-
+bool isWIFIOn = false;
 uint8_t responseArray[1024];
 String user;
 String password;
@@ -21,8 +21,8 @@ int responseIndex = 0;
 
 const uint ServerPort = 5679;
 WiFiServer wifiServer(ServerPort);
-const char * ssid = "reedy-home";
-const char * wifiPassword = "uOzb1rmjc1";
+const char * ssid = "ATT 2.4";
+const char * wifiPassword = "fennig1996";
 String ipAddress = "";
 
 //timeout 300000 is 5 minutes in milliseconds
@@ -30,21 +30,58 @@ String ipAddress = "";
 const int CONNECTION_TIMEOUT = 30000;
 BluetoothSerial SerialBT;
 
+/********************************************************
+*                      Utility                          *
+********************************************************/
 
-void initializeWifi() {
-    // WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, wifiPassword);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("WiFi Failed");
-        while(1) {
-            delay(1000);
-        }
-    }
-    Serial.println("\nConnected to the WiFi network");
-    Serial.println(WiFi.localIP());
-    ipAddress = (String) WiFi.localIP().toString();
-    wifiServer.begin();
+JSONVar parseStringtoJSON(String s){
+  JSONVar j = JSON.parse(s);
+  if(JSON.typeof(j) == "undefined"){
+    Serial.println("Parsing input failed!");
+    return JSON.parse("{}");
+  }
+  return j;
 }
+
+
+String parseResponseToString(){
+    Serial.println("\nReceived Data: ");
+    String resp = uintConvertToString(responseArray, responseIndex);
+    //print to serial
+    Serial.println(resp);
+    
+    return resp;
+    //send string
+//    SerialBT.print(resp);
+
+    //old code
+    //response where each index contains ascii of character
+//    Serial.write(&responseArray[0], responseIndex);
+    //echo response back to android
+//    SerialBT.write(&responseArray[0], responseIndex);
+}
+
+
+String uintConvertToString(uint8_t* a, int arraySize){
+    int i;
+    String s = "";
+    for (i = 0; i < arraySize; i++) {
+        s = s + (char) a[i];
+    }
+    return s;
+}
+
+
+void freeArrays(){
+  //cleanup
+    responseIndex = 0;
+    //free array memory
+    memset(responseArray, 0, 1024);
+}
+
+/********************************************************
+*                     Bluetooth                         *
+********************************************************/
 
 void handleBluetooth(){
   //if there was data sent to esp32
@@ -62,82 +99,33 @@ void handleBluetooth(){
   counter+=20;
 }
 
-String parseResponseToString(){
-    Serial.println("\nReceived Data: ");
-    String resp = uintConvertToString(responseArray, responseIndex);
-    //print to serial
-    Serial.println(resp);
-    
-    return resp;
-    //send string
-    //SerialBT.print(resp);
-
-    //old code
-    //response where each index contains ascii of character
-    //Serial.write(&responseArray[0], responseIndex);
-    //echo response back to android
-    //SerialBT.write(&responseArray[0], responseIndex);
-    
-    
-}
-
-String uintConvertToString(uint8_t* a, int arraySize)
-{
-    int i;
-    String s = "";
-    for (i = 0; i < arraySize; i++) {
-        s = s + (char) a[i];
-    }
-    return s;
-}
-
-void freeArrays(){
-  //cleanup
-    responseIndex = 0;
-    //free array memory
-    memset(responseArray, 0, 1024);
-}
-
-void endBluetoothConnection(){
-  isBluetoothOn = false;
-    Serial.println("Closing bluetooth connection!");
-    SerialBT.flush();  
-    SerialBT.disconnect();
-    SerialBT.end();
-    digitalWrite(LED_PIN, LOW);
-}
 
 void sendResponseToBluetooth(String message){
-  uint8_t u = 0;
-  uint8_t p = 0;
-  JSONVar myObject = JSON.parse(message);
-
-  if (JSON.typeof(myObject) == "undefined") {
-    Serial.println("Parsing input failed!");
-  }
+  JSONVar myObject = parseStringtoJSON(message);
 
   if (myObject.hasOwnProperty("username")) {
-    //Serial.print("myObject[\"username\"] = ");
-    //Serial.println(myObject["username"]);
+//    Serial.print("myObject[\"username\"] = ");
+//    Serial.println(myObject["username"]);
     user = myObject["username"];
     Serial.print("\nUsername set");
   }
 
   
   if (myObject.hasOwnProperty("password")) {
-    //Serial.print("myObject[\"password\"] = ");
-    //Serial.println(myObject["password"]);
+//    Serial.print("myObject[\"password\"] = ");
+//    Serial.println(myObject["password"]);
     password = myObject["password"];
     Serial.print("\nPassword set");
 
   }
-  int ledValue = digitalRead(BUTTON_PIN);
+  uint8_t ledValue = digitalRead(BUTTON_PIN);
   String messageResponse = "{\"name\": \"" + deviceName +  "\", \"ipAddress\": \"" + ipAddress + "\", \"initialState\": " + (String) ledValue +  "}";
   SerialBT.println(messageResponse);
   Serial.print("\nSent response");
-  //String messageResponse = "{recieved_user:  test, recieved_password: alsotest}";
+//  String messageResponse = "{recieved_user:  test, recieved_password: alsotest}";
  
 }
+
 
 void readData(){
     //read character
@@ -156,9 +144,71 @@ void readData(){
       sendResponseToBluetooth(jsonMessage);
       //send response
       freeArrays(); 
+    }    
+}
+
+
+void endBluetoothConnection(){
+  isBluetoothOn = false;
+    Serial.println("Closing bluetooth connection!");
+    SerialBT.flush();  
+    SerialBT.disconnect();
+    SerialBT.end();
+    digitalWrite(LED_PIN, LOW);
+}
+
+/********************************************************
+*                        Wi-Fi                          *
+********************************************************/
+
+void initializeWifi() {
+    // WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, wifiPassword);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("WiFi Failed");
+        while(1) {
+            delay(1000);
+        }
     }
-    
+    Serial.println("\nConnected to the WiFi network");
+    Serial.println(WiFi.localIP());
+    ipAddress = (String) WiFi.localIP().toString();
+    wifiServer.begin();
+    isWIFIOn = true;
+}
+
+
+//connects, handles request, kills connection
+void handleConnections(){
+  WiFiClient client = wifiServer.available();
+
+  if(client){
+    while(client.connected()){
+      while(client.available() > 0){
+        JSONVar jReq = parseStringtoJSON(client.readString());
+        handleRequest(jReq, client);
+      }
+      client.stop();
+    }
   }
+}
+
+
+//Checks username, password, and handles message if correct
+void handleRequest(JSONVar req, WiFiClient client){
+  if(req["username"] == user && req["password"] == password){
+    switch((int) req["message"]){
+      case 0:
+      case 1: 
+        digitalWrite(LED_PIN, (int) req["message"]);
+      default:
+        client.write(digitalRead(LED_PIN));       
+    }
+  }
+}
+/********************************************************
+*                        Main                           *
+********************************************************/
 
 void setup() {
   Serial.begin(9600);               // initialize serial
@@ -182,7 +232,8 @@ void loop() {
   if(isBluetoothOn){
     handleBluetooth();
   }
-
-  
-  
+  //WiFi is initialized
+  if(isWIFIOn){
+    handleConnections();
+  }
 }
